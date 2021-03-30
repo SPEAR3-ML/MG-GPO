bf45fc29cf1882e0c2237aa98f0fa7f07a8065d8
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-03-30 01:25:44 -0700

    Clean up the code Change the switch useTeeport to useGPML, remove trace of Teeport due to the daunting setup

--- run.m
+++ run.m
@@ -8,42 +8,21 @@ addpath evaluators;
 addpath utils;
 
 %% Configs
-problem = @Rosenbrock;%@ZDT2;
-Nobj = 1;
-Nvar = 4;
-Npop = 80;
-Ngen = 15;
-useTeeport = 1;
+problem = @ZDT2;
+Nobj = 2;
+Nvar = 30;
+Npop = 10;
+Ngen = 10;
+useGPML = 0;
+
 %% Run
-if useTeeport == 0
-    % Read the platform settings
-    fid = fopen('.teeport');
-    url = fgetl(fid);
-    if url == -1
-        fclose(fid);
-        error('Cannot read the url of the platform.')
-    end
-    processorId = fgetl(fid);
-    if processorId == -1
-        fclose(fid);
-        error('Cannot read the id of the GPy processor on the platform.')
-    end
-    fclose(fid);
-    
-    % Connect to the platform
-    teeport = Teeport(url);
-    evaluate = teeport.useEvaluator(problem);
-    predict = teeport.useProcessor(processorId);
-    
-    gbest = MGGPO(evaluate,predict,Npop,Ngen,Nobj,Nvar);
-    teeport.cleanUp(); % disconnect from the platform
-elseif useTeeport == 1
+if useGPML == 0
     addpath GP;
     evaluate = problem;
     predict = @GP_predict;
     
     gbest = MGGPO(evaluate,predict,Npop,Ngen,Nobj,Nvar);
-elseif useTeeport == 2
+elseif useGPML == 1
     addpath GPML;
     evaluate = problem;
     predict = @GPML_predict;
